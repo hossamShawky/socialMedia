e878fe0400039f7fde34c722f4557c44d5436b51
Hossam M.Shawky <52916569+hossamShawky@users.noreply.github.com>
2024-06-03 13:16:09 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => '44.193.211.190',
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
